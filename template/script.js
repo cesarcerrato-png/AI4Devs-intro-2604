baf7ea6a723d5d7cfb1fb3709c46fd7c720a2f55
cesarcerrato-png <cesar.cerrato@gruposolutia.com>
2026-05-05 14:53:25 +0200

    Update script.js

---
+++ template/script.js
@@ -0,0 +1,23 @@
+const textInput = document.getElementById('textInput');
+const reverseButton = document.getElementById('reverseButton');
+const result = document.getElementById('result');
+
+function reverseString(value) {
+    return value.split('').reverse().join('');
+}
+
+reverseButton.addEventListener('click', () => {
+    reverseButton.classList.add('pressed');
+    const reversed = reverseString(textInput.value);
+    result.textContent = reversed || 'Reversed text appears here.';
+
+    setTimeout(() => {
+        reverseButton.classList.remove('pressed');
+    }, 150);
+});
+
+textInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        reverseButton.click();
+    }
+});
